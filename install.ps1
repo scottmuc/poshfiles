934bbb35d6cbb81096349dced8b8469019acd756
Scott Muc <scottmuc@gmail.com>
2011-08-19 13:11:13 -0700

    Fixed file copy issues

--- install.ps1
+++ install.ps1
@@ -1,37 +1,44 @@
 $temp_file = "$($env:Temp)\poshfiles.zip"
 
-function New-Directory {
-    param($dir)
-    if ((Test-Path $dir)) {
-        Resolve-Path $dir | Remove-Item -Recurse
-    }
-    New-Item $dir -Type Container
-}
-
 function Get-PoshfilesZip {
-    (new-object Net.WebClient).DownloadFile("https://github.com/scottmuc/poshfiles/zipball/master", $temp_file)
+    param ($file)
+    if (Test-Path $file) {
+        Remove-Item $file | Out-Null
+    }
+    "Downloading poshfiles from github" | Write-Host
+    (new-object Net.WebClient).DownloadFile("https://github.com/scottmuc/poshfiles/zipball/master", $file)
 }
 
 function Unzip-PoshfilesZip {
     Resolve-Path $env:Temp\*-poshfiles-* | Remove-Item -Recurse
 
+    "Extracting poshfiles" | Write-Host
     $shell_app = new-object -com shell.application 
     $zip_file = $shell_app.namespace($temp_file) 
     $destination = $shell_app.namespace($env:Temp) 
     $destination.Copyhere($zip_file.items())
 
-    New-Directory $env:UserProfile\poshfiles
-    Resolve-Path $env:Temp\*-poshfiles-*\* | Copy-Item -Dest $env:UserProfile\poshfiles
+    if ((Test-Path $env:UserProfile\poshfiles)) {
+        Resolve-Path  $env:UserProfile\poshfiles | Remove-Item -Recurse
+    }
+
+    New-Item  $env:UserProfile\poshfiles -Type Container | Out-Null
+    $unzip_dir = (Resolve-Path $env:Temp\*-poshfiles-* ).Path
+    Copy-Item $unzip_dir\* -Dest $env:UserProfile\poshfiles -Recurse
 }
 
-function Install-Poshfiles {
-    $profileDir = Split-Path $profile
-    New-Directory $profileDir
+function Setup-Profile {
+    $profile_dir = Split-Path $profile
+    if (-not (Test-Path $profile_dir)) {
+        New-Item $profile_dir -Type Container | Out-Null
+    }
+    "Setting up profile" | Write-Host
     ". `$env:Userprofile\poshfiles\profile\profile.ps1" | Out-File $profile
 }
 
-Get-PoshfilesZip
+Get-PoshfilesZip $temp_file
 Unzip-PoshfilesZip
-Install-Poshfiles
+Setup-Profile
 
+. $profile
 "Custom poshfiles installed!" | Write-Host -Fore Green
